7e0ac125aca5a43262e85f23111101f73c37e4bc
Najib Amoussaoui <namoussa@c4r1p8.1337.ma>
2025-07-11 11:56:22 +0100

    welcome page with responsivness

--- my-project/src/main.ts
+++ my-project/src/main.ts
@@ -1,10 +1,11 @@
 import './style.css';
-import { renderHome } from './renderHome';
+import { renderWeclome } from './renderWeclome';
+// import { renderSing } from './renderSing';
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
 app.innerHTML = `<div id="root"></div>`;
 
 const root = document.getElementById('root');
 if (root) {
-  renderHome(root);
+  renderWeclome(root);
 }
---
+++ my-project/src/renderSing.ts
@@ -0,0 +1,7 @@
+export function renderSing(container: HTMLElement) {
+ container.innerHTML = `
+ <div>
+  <h1 class="text-red-500 ">hello</h1>
+ </div>
+ `;
+}
