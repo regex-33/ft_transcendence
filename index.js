8cc24d75ad30ed8dc54c656a26499967bd9aed96
Ahmed Ghlimi <aghlimi@e1r5p3.1337.ma>
2025-07-12 20:25:00 +0100

    edit banner and logger

--- index.js
+++ index.js
@@ -1,82 +1,24 @@
 const fastify = require("fastify")();
 const db = require("./models");
-const { request } = require("undici");
-
+const banner = require("./middleware/banner");
 const path = require("path");
 const fastifyStatic = require("@fastify/static");
 const { UserRoutes, FriendRoutes, OauthRoutes,  checkCodeRoutes} = require("./Router");
-const { Console } = require("console");
+const logger = require("./middleware/logger");
 
-fastify.addHook("onResponse", async (request, reply) => {
-  console.log(
-    `[${new Date().toISOString()}] ${request.method} ${request.url}  - ${
-      reply.statusCode
-    }`
-  );
-});
+fastify.addHook("onResponse", logger);
+fastify.addHook("onRequest", banner);
 
-const arrayOfClients = new Map();
-const arrayOfBannedClients = new Set();
-const CLEANUP_INTERVAL = Number(process.env.CLEANUP_INTERVAL) || 300000; 
-let lastCleanup = Date.now();
-
-fastify.addHook("onRequest", (request, reply, done) => {
-  const now = Date.now();
-  const clientIp = request.ip;
-
-  if (arrayOfBannedClients.has(clientIp)) {
-    return reply.status(403).send({ error: "You are banned" });
-  }
-
-  if (now - lastCleanup > CLEANUP_INTERVAL) {
-    arrayOfClients.clear();
-    lastCleanup = now;
-  }
-
-  const TIME_BTWN_REQUESTS = Number(process.env.TIME_BTWN_REQUESTS) || 3000;
-  const MAX_REQUESTS = Number(process.env.NUMBER_OF_REQUESTS_TO_BAN) || 10;
-
-  const client = arrayOfClients.get(clientIp);
-  if (client) {
-    if (now - client.time < TIME_BTWN_REQUESTS) {
-      client.count++;
-    } else {
-      client.count = 1;
-      client.time = now;
-    }
-
-    if (client.count >= MAX_REQUESTS) {
-      arrayOfBannedClients.add(clientIp);
-      return reply.status(429).send({ error: "Too many requests - Banned" });
-    }
-
-    arrayOfClients.set(clientIp, client);
-  } else {
-    arrayOfClients.set(clientIp, {
-      time: now,
-      count: 1,
-    });
-  }
-
-  done(); 
-});
-
-
-fastify.get("/", (req, reply) => {
-  reply.send({ message: "Welcome to the Transcendence API" });
-});
-
-fastify.register(fastifyStatic, {
-  root: path.join(__dirname, "uploads"),
-  prefix: "/uploads/",
-});
 
 fastify.register(require("@fastify/multipart"));
-
 fastify.register(UserRoutes, { prefix: "/api" });
 fastify.register(FriendRoutes, { prefix: "/api" });
 fastify.register(OauthRoutes, { prefix: "/api" });
 fastify.register(checkCodeRoutes, { prefix: "/api" });
+fastify.register(fastifyStatic, {
+  root: path.join(__dirname, "uploads"),
+  prefix: "/uploads/",
+});
 
 db.sequelize
   .sync()
