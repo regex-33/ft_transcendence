f431f3da69d346c831b34fc4dad876a2fc689834
Aghlimi <ahmed4aghlimi@gmail.com>
2025-09-08 11:21:09 +0100

    fix blocked filter

--- services/user-service/Router/Friends/block.js
+++ services/user-service/Router/Friends/block.js
@@ -4,21 +4,24 @@ const { fillObject } = require("../../util/logger");
 
 const blockUser = async (req, reply, payload, userId, username) => {
   try {
-    const users = await Promise.all([
-      db.User.findOne({ where: { username } }),
-      db.User.findByPk(userId)
-    ]);
-    if (users.filter(user => user).length != 2) {
-      fillObject(req, 'WARNING', 'block', payload.username, false, 'user not found', req.cookies?.token || null);
-      return reply.stats(404).send({ 'message': 'user not found' });
+    const rel = await db.Relationship.findOne({
+      where: {
+        [db.Sequelize.Op.or]: [
+          { userId: userId },
+          { otherId: userId }
+        ]
+      }
+    });
+    if (!rel) {
+      return reply.status(404).send({ 'message': 'this user not friend.' });
     }
-    await users[1].addOther(users[0], { through: { status: 'blocked' } });
-    fillObject(req, 'INFO', 'block', payload.username, true, null, req.cookies?.token || null);
+    rel.status = 'blocked';
+    await rel.save();
     return reply.send({ 'message': 'success' });
+
   } catch (error) {
-    fillObject(req, 'ERROR', 'block', payload.username, false, error.message, req.cookies?.token || null);
     console.log('block crash:', error);
-    return reply.stats(500).send({ error: 'internal server error' })
+    return reply.status(500).send({ error: 'internal server error' });
   }
 };
 
