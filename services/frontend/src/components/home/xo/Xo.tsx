baa96014b9ea7fd3c79e837467e16e40d9755acd
Aghlimi <ahmed4aghlimi@gmail.com>
2025-09-14 15:50:17 +0100

    provid player vs player option

--- services/frontend/src/components/home/xo/Xo.tsx
+++ services/frontend/src/components/home/xo/Xo.tsx
@@ -2,15 +2,16 @@ import { useEffect } from "../../../hooks/useEffect";
 import { useState } from "../../../hooks/useState";
 import { ComponentFunction, VNode } from "../../../types/global";
 import { h } from "../../../vdom/createElement";
-let turn = !true;
-let reset_class = "w-full h-20 text-white text-2xl font-['Irish_Grover'] text-4xl ";
-reset_class += "bg-[url(/images/home-assests/bg-gameMode.svg)] bg-no-repeat bg-[length:100%_100%] ";
+let turn = 'X';
+let aimode = false;
+let reset_class = "";
 let table_class = " bg-[url(/images/xo/xo.png)] bg-no-repeat bg-[length:100%_100%] ";
 table_class += " w-full h-[70%] ";
 table_class += "  m-auto justify-self-end ";
 let container_class = "w-full h-full flex flex-col  ";
 let td_class = "border-5 bg-no-repeat bg-[length:50%_50%]";
 td_class += " bg-center"
+let ai_class = "w-full h-20 text-white text-2xl font-['Irish_Grover'] text-4xl ";
 async function safeFetch(url: string, options: any) {
     try {
         const res = await fetch(url, options);
@@ -21,24 +22,28 @@ async function safeFetch(url: string, options: any) {
     }
 }
 
-const click = (i: number, j: number, setMap: any) => {
-    safeFetch('/xo-game/play', {
-        method: 'POST',
-        credentials: "include",
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            cell: {
-                x: i,
-                y: j
-            }
-        })
-    }).then(data => {
-        if (data?.map)
-            setMap(data.map)
+const click = (x: number, y: number, setMap: any, map: any) => {
+    if (aimode) {
+        safeFetch('/xo-game/play', {
+            method: 'POST',
+            credentials: "include",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ cell: { x: y, y: x } })
+        }).then(data => {
+            if (data?.map)
+                setMap(data.map)
 
-    }).catch(error => console.log("error"))
+        }).catch(error => console.log("error"))
+    }
+    else if (map[x][y] === '-') {
+        let new_map = [[...map[0]], [...map[1]], [...map[2]]]
+
+        new_map[x][y] = turn;
+        turn = turn == 'X' ? 'O' : 'X';
+        setMap(new_map);
+    }
 
 };
 
@@ -80,16 +85,13 @@ const Xo: ComponentFunction = () => {
         };
         datafetching();
     }, [])
-    for (let i = 0; i < 3; i++) {
+    for (let y = 0; y < 3; y++) {
         const tr = <tr></tr>;
-        for (let j = 0; j < 3; j++) {
+        for (let x = 0; x < 3; x++) {
             const td = <td
-                id={`cell-${i}-${j}`}
-                onclick={() => click(j, i, setMap)}
-                className={`bg-[url(/images/xo/${(() => {
-                    if (turn && map[i][j] != '-') return map[i][j] == 'X' ? 'O' : 'X';
-                    return map[i][j];
-                })()}.png)] ${td_class}`}
+                id={`cell-${x}-${y}`}
+                onclick={() => click(x, y, setMap, map)}
+                className={`bg-[url(/images/xo/${map[x][y]}.png)] ${td_class}`}
             >
             </td>;
             tr.children.push(td);
@@ -97,13 +99,32 @@ const Xo: ComponentFunction = () => {
         table.children.push(tr);
     }
     return <div className={container_class}>
-        <button
-            type="button"
-            className={reset_class}
-            onclick={() => reset(setMap)}
-        >
-            Tic tac Toe
-        </button>
+        <div className="w-full relative flex justify-center ">
+            <button
+
+                type="button"
+                className={`bg-[url(/images/xo/ai.png)] w-[52px] h-[52px] absolute top-4 left-0 bg-center bg-no-repeat ${ai_class}`}
+                onclick={() => {
+                    aimode = true;
+                    reset(setMap);
+                }}>
+            </button>
+            <button
+                type="button"
+                className={`bg-[url(/images/xo/title.png)] font-['Irish_Grover']   w-[200px] h-20 absolute top-[-20px]  bg-center bg-no-repeat  ${reset_class}`}
+                onclick={() => reset(setMap)}>
+                Tic tac Toe
+            </button>
+            <button
+                type="button"
+                className={`bg-[url(/images/xo/local.png)] w-[52px] h-[52px] absolute top-4 right-0 bg-center bg-no-repeat`}
+                onclick={() => {
+                    aimode = false;
+                    setMap([['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']]);
+                }}
+            >
+            </button>
+        </div>
         {table}
     </div>;
 }
