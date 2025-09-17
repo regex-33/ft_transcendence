78ca5d9a1ac70f9567007e0948a0484397142537
Badr Chanaa <badrchanaa@gmail.com>
2025-09-17 14:23:01 +0100

    schema models

--- services/game-service/src/routes/game.ts
+++ services/game-service/src/routes/game.ts
@@ -1,29 +1,30 @@
-import type { FastifyInstance, FastifyPluginOptions } from "fastify";
+import type { FastifyInstance } from "fastify";
 import { createGameSchema, getGameSchema } from "../schemas";
 import { createGame, getGame } from "../controllers/gameController";
-import { createPlayer } from "../controllers/playerController";
+//import { createPlayer } from "../controllers/playerController";
+import { GameStatus, GameType } from "../../generated/prisma";
 
-async function gameRoutes(fastify: FastifyInstance, opts: FastifyPluginOptions) {
-	opts;
-	fastify.post('/create', { schema: createGameSchema }, async (request, reply) => {
+async function gameRoutes(fastify: FastifyInstance) {
+	fastify.post<{ Body: { status: GameStatus, type: GameType } }>('/create', { schema: createGameSchema }, async (request, reply) => {
 		const { gameStatus, gameType } = request.body as any;
 		const db = fastify.prisma;
-		const player = await createPlayer(db, { id: 10 });
-		const game = await createGame(db, { status: gameStatus, type: gameType, playerId: player.userId });
+		// const player = await createPlayer(db, { id: 10 });
+		const userId = 10;
+		const game = await createGame(db, { status: gameStatus, type: gameType, playerId: userId });
 		reply.code(201).send(game);
 	});
 
-	fastify.post('/delete', async (request, reply) => {
-		return { "hello": "world" };
-	});
+	// fastify.post('/delete', async (request, reply) => {
+	//	return { "hello": "world" };
+	// });
 
-	fastify.get('/:id', {
+	fastify.get<{ Params: { id: number } }>('/:id', {
 		schema: getGameSchema
 	}, async (request, reply) => {
 		const { id } = request.params;
 		const game = getGame(fastify.prisma, id);
 		if (!game)
-			return reply.code(404).send({"error": "game not found"});
+			return reply.code(404).send({ "error": "game not found" });
 		reply.code(200).send(game);
 	});
 }
